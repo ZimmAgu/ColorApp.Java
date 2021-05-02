bbca9883f2d21bcb855fc52ccfb219149387f7aa
Zim Agu <zimmagu@gmail.com>
2021-05-01 23:52:36 -0700

    Added main windoe

---
+++ ColorApp.java
@@ -0,0 +1,13 @@
+// Written by Zimuzo Agu
+
+import javax.swing.*;
+
+public class ColorApp {
+    public static void main (String argv []) {
+
+    JFrame frame = new JFrame("Color Sampler");
+    frame.setSize(500, 500);
+    frame.setVisible(true);
+
+    }
+}
